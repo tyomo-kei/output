ef9d2d1f52d49c71d71dc01e1184f1e6d9b29a14
長友啓祐 <zhangyouqiyou@gmail.com>
2022-03-07 08:51:51 +0900

    Rubocopの導入

--- app/models/resolution.rb
+++ app/models/resolution.rb
@@ -7,7 +7,7 @@ class Resolution < ApplicationRecord
   belongs_to :user
 
   def self.search(search)
-    if search != ""
+    if search != ''
       Resolution.where('erroe_message LIKE(?) OR status LIKE(?)', "%#{search}%", "%#{search}%")
     else
       Resolution.all
