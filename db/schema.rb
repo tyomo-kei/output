afe2996e3a6843b3b200a65d24ca38c61c8b4ef4
長友啓祐 <zhangyouqiyou@gmail.com>
2022-03-16 18:41:16 +0900

    エラー解決投稿のプレビュー画面の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_10_020836) do
+ActiveRecord::Schema.define(version: 2022_03_16_092719) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title", null: false
@@ -21,11 +21,7 @@ ActiveRecord::Schema.define(version: 2022_03_10_020836) do
   end
 
   create_table "resolutions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "erroe_message"
-    t.string "language"
-    t.text "status"
-    t.text "code"
-    t.text "cauce"
+    t.text "status", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
